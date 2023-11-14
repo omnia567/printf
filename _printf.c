f90e33915b0ac348b2ec2bb582cdd64d65f2222c
Omnia Ali <90191487+omnia567@users.noreply.github.com>
2023-11-14 23:09:44 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,18 +2,17 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 
-/
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+  /
+   * _printf - Printf function
+   * @format: format.
+   * Return: Printed chars.
+   */
 int _printf(const char *format, ...)
 {
   int i, p = 0, p_ch = 0;
   int f, w, precn, s, buff_ind = 0;
   va_list list;
   char buffer[BUFF_SIZE];
-
   if (format == NULL)
     return (-1);
 
@@ -52,11 +51,11 @@ int _printf(const char *format, ...)
   return (p_ch);
 }
 
-/
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
+  /
+   * print_buffer - Prints the contents of the buffer if it exist
+   * @buffer: Array of chars
+   * @buff_ind: Index at which to add next char, represents the length.
+  */
 void print_buffer(char buffer[], int *buff_ind)
 {
   if (*buff_ind > 0)
