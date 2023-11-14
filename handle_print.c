7c970418f8003ac2ad6bb6f8a59f7691806a7b8f
Omnia Ali <90191487+omnia567@users.noreply.github.com>
2023-11-14 19:29:34 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -20,7 +20,8 @@ int H_print(const char *ft, int *ind, va_list list, char buffer[],
 		{'i', print_i}, {'d', print_i}, {'b', print_b},
 		{'u', print_unsned}, {'o', print_o}, {'x', ‎print_hexaD},
 		{'X', print_hexaUpper}, {'p', print_PTR}, {'S', print_non_print‎},
-		{'r', print_reve‎}, {'R', print_rot13str‎}, {'\0', NULL}
+		{'r', print_reve‎}, {'R', print_rot13str‎}
+
 	};
 	for (i = 0; ft_types[i].ft != '\0'; i++)
 		if (ft[*ind] == ft_types[i].ft)
