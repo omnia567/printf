e0b0beb9a6b88b6d7c250a9336a374f041da4c8d
Omnia Ali <omniaali073@gmail.com>
2023-11-13 22:34:42 -0800

    j

--- _printf.c
+++ _printf.c
@@ -1,66 +1,55 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
 
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+int _printf(const char *format, ...) {
+    va_list args;
+    int printed_chars = 0;
 
-	*buff_ind = 0;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format != '%') {
+            _putchar(*format);
+            printed_chars++;
+        } else {
+            format++;
+            if (*format == '\0') break;
+
+            switch (*format) {
+                case 'c': {
+                    char c = va_arg(args, int);
+                    _putchar(c);
+                    printed_chars++;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str) {
+                        _putchar(*str);
+                        str++;
+                        printed_chars++;
+                    }
+                    break;
+                }
+                case '%':
+                    _putchar('%');
+                    printed_chars++;
+                    break;
+                default:
+                    _putchar('%');
+                    _putchar(*format);
+                    printed_chars += 2;
+                    break;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+    return printed_chars;
 }
+
