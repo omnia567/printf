fdb8d5376e13a2b93835fe186e6c47477c51ae8e
Omnia Ali <omniaali073@gmail.com>
2023-11-14 09:05:08 -0800

    handleprint

--- get_width.c
+++ get_width.c
@@ -10,27 +10,27 @@
  */
 int get_w(const char *format, int *i, va_list list)
 {
-  int curr_i;
-  int w = 0;
+	int curr_i;
+	int w = 0;
 
-  for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-  {
-    if (isDigit(format[curr_i]))
-    {
-      w *= 10;
-      w += format[curr_i] - '0';
-    }
-    else if (format[curr_i] == '*')
-    {
-      curr_i++;
-      w = va_arg(list, int);
-      break;
-    }
-    else
-      break;
-  }
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+	if (isDigit(format[curr_i]))
+	{
+	w *= 10;
+	w += format[curr_i] - '0';
+	}
+	else if (format[curr_i] == '*')
+	{
+	curr_i++;
+	w = va_arg(list, int);
+	break;
+	}
+	else
+	break;
+	}
 
-  *i = curr_i - 1;
+	*i = curr_i - 1;
 
-  return (w);
+	return (w);
 }
