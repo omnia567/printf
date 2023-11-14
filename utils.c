47ca388764feb033d7740fae48ccddebdec7aa29
Omnia Ali <90191487+omnia567@users.noreply.github.com>
2023-11-14 21:39:20 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-int is_print(char c)
+int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
@@ -21,7 +21,7 @@ int is_print(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int appendHexaCode(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
